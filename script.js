bf9df646850a0c9ed5077dc6c3d8c68dd9b42cec
Chris Gartsen.nl <chris@gartsen.nl>
2024-09-02 21:39:03 +0200

    Add mobile menu to login page

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const modalButton = document.getElementById("showModal");
-const modalBackdrop = document.getElementsByClassName("modal-backdrop")[0];
-const modal = document.getElementsByClassName("modal")[0];
-const modalClose = document.getElementsByClassName("modal-close")[0];
+// const modalButton = document.getElementById("showModal");
+// const modalBackdrop = document.getElementsByClassName("modal-backdrop")[0];
+// const modal = document.getElementsByClassName("modal")[0];
+// const modalClose = document.getElementsByClassName("modal-close")[0];
 
-modalButton.addEventListener("click", function (e) {
-  modalBackdrop.classList.add("show");
-  modal.classList.add("show");
-});
+// modalButton.addEventListener("click", function (e) {
+//   console.log("Hello");
+//   modalBackdrop.classList.add("show");
+//   modal.classList.add("show");
+// });
 
-modalClose.addEventListener("click", function (e) {
-  modalBackdrop.classList.remove("show");
-  modal.classList.remove("show");
+// modalClose.addEventListener("click", function (e) {
+//   console.log("Closing");
+//   modalBackdrop.classList.remove("show");
+//   modal.classList.remove("show");
+// });
+
+const menuButton = document.getElementById("showMenuButton");
+const menu = document.getElementById("mobile-menu");
+
+menuButton.addEventListener("click", function (e) {
+  console.log("Showing Mobile Menu");
+  menu.classList.toggle("active");
+  menuButton.classList.toggle("active");
 });
